0d0d97460131125b2ec0205c5e52a4fb29043872
neerajdhurandher <neerajdhurandar@gmail.com>
2022-02-11 23:52:42 +0530

    navigate between two fragments using Fragment manager & Fragment Transaction.

---
+++ app/src/main/java/com/example/kotlinapptrail/FirstFragment.kt
@@ -0,0 +1,42 @@
+package com.example.kotlinapptrail
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.kotlinapptrail.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding : FragmentFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_first,container,false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextButton.setOnClickListener {
+
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.fragmentContainer,SecondFragment())
+            transaction?.commit()
+        }
+    }
+}
--- app/src/main/java/com/example/kotlinapptrail/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapptrail/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlinapptrail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.kotlinapptrail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.button1.setOnClickListener {
+            val manager: FragmentManager = supportFragmentManager
+            val transaction: FragmentTransaction = manager.beginTransaction()
+            transaction.replace(R.id.fragmentContainer,FirstFragment())
+            transaction.commit()
 
-            binding.text1.text = getString(R.string.welcome_text)
 
-        }
+
     }
 }
